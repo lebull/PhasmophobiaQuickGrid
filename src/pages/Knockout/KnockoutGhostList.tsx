ea0e3b7c9336c0ca2a176eea77ae9bc274ff64c0
Lebull <bulldogboy912000@yahoo.com>
2020-10-10 18:23:16 -0500

    Hide ghosts on knockout

--- src/pages/Knockout/KnockoutGhostList.tsx
+++ src/pages/Knockout/KnockoutGhostList.tsx
@@ -1,26 +1,37 @@
 import React from "react";
 import { GhostType, TestType } from "../../data/types";
 import { GhostTypes } from "../../data/adhoc";
+import { getPossibleGhostTypesFromActiveTests } from "./Helpers";
 
 export interface IKnockoutGhostListProps {
     activeTests: Array<TestType>;
     className?: string;
 }
 
-export const KnockoutGhostList = ({className}: IKnockoutGhostListProps) => {
+export const KnockoutGhostList = ({activeTests, className}: IKnockoutGhostListProps) => {
+
+    let possibleGhosts = getPossibleGhostTypesFromActiveTests(activeTests);
+
     return <div className={`columns is-multiline ${className}`}>
-        { GhostTypes.map((ghostType: GhostType) => <KnockoutGhost ghostType={ghostType} />) }
+        { GhostTypes.map((ghostType: GhostType) => 
+            <KnockoutGhost 
+                ghostType={ghostType}
+                disabled={!possibleGhosts.includes(ghostType)}
+            />) 
+        }
     </div>
 }
 
 interface IKnockoutGhostProps {
-    ghostType: GhostType
+    ghostType: GhostType,
+    disabled?: boolean
 }
 
-const KnockoutGhost = ({ghostType} : IKnockoutGhostProps) => {
-    return <div className="column is-one-third card">
-        <div className="card-content">
+const KnockoutGhost = ({ghostType, disabled=false} : IKnockoutGhostProps) => {
+    return <div className={`column is-one-third ${disabled? "is-hidden" : ""}`}>
+        <div className="box is-primary is-fullwidth">
             <h4 className="title is-4">{ghostType.name}</h4>
+            <p>{ghostType.description}</p>
         </div>
     </div>
 }
