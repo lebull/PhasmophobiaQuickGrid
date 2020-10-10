89f664b8e569a36a0612459dcd993e151b04fe7a
Lebull <bulldogboy912000@yahoo.com>
2020-10-10 17:00:46 -0500

    Remove dists

--- src/pages/dist/Home.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Home = void 0;
-var react_1 = require("react");
-var KnockoutList_1 = require("./KnockoutList");
-exports.Home = function () {
-    return react_1["default"].createElement(KnockoutList_1.KnockoutList, null);
-};
