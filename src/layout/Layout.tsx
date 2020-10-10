dbde3f5a6a47e7034e84b7546bf03028656e05f5
lebull <bulldogboy912000@yahoo.com>
2020-10-10 11:50:23 -0500

    Toss in top navbar

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const Header = () =>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+            <a className="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28"></img>
+            </a>
+
+            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div className="navbar-menu">
+            <div className="navbar-start">
+                <Link to="/" className="navbar-item">Home</Link>
+            </div>
+        </div>
+    </nav>
+
+
 interface ILayoutProps {
     children: any;
 }
 
 export const Layout = ({ children } : ILayoutProps) => 
     <div>
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-            </ul>
-        </div>
-            { children }
+        <Header/>
+        { children }
         <div>Footer</div>
     </div>  
--- src/layout/dist/Layout.js
+++ src/layout/dist/Layout.js
@@ -3,13 +3,23 @@ exports.__esModule = true;
 exports.Layout = void 0;
 var react_1 = require("react");
 var react_router_dom_1 = require("react-router-dom");
+var Header = function () {
+    return react_1["default"].createElement("nav", { className: "navbar", role: "navigation", "aria-label": "main navigation" },
+        react_1["default"].createElement("div", { className: "navbar-brand" },
+            react_1["default"].createElement("a", { className: "navbar-item", href: "https://bulma.io" },
+                react_1["default"].createElement("img", { src: "https://bulma.io/images/bulma-logo.png", alt: "logo", width: "112", height: "28" })),
+            react_1["default"].createElement("a", { role: "button", className: "navbar-burger burger", "aria-label": "menu", "aria-expanded": "false", "data-target": "navbarBasicExample" },
+                react_1["default"].createElement("span", { "aria-hidden": "true" }),
+                react_1["default"].createElement("span", { "aria-hidden": "true" }),
+                react_1["default"].createElement("span", { "aria-hidden": "true" }))),
+        react_1["default"].createElement("div", { className: "navbar-menu" },
+            react_1["default"].createElement("div", { className: "navbar-start" },
+                react_1["default"].createElement(react_router_dom_1.Link, { to: "/", className: "navbar-item" }, "Home"))));
+};
 exports.Layout = function (_a) {
     var children = _a.children;
     return react_1["default"].createElement("div", null,
-        react_1["default"].createElement("div", null,
-            react_1["default"].createElement("ul", null,
-                react_1["default"].createElement("li", null,
-                    react_1["default"].createElement(react_router_dom_1.Link, { to: "/" }, "Home")))),
+        react_1["default"].createElement(Header, null),
         children,
         react_1["default"].createElement("div", null, "Footer"));
 };
