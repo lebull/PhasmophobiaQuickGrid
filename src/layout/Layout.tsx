6b8f56b7e4a58924ae0b852a411ad9401de782d2
lebull <bulldogboy912000@yahoo.com>
2020-10-11 11:48:10 -0500

    Styling

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,17 @@
-import { faGhost } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGhost } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () =>
-    <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
             <a className="navbar-item px-i" href="/">
-            {/* <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28"></img> */}
-                <span className="icon">
+                <img src="/android-chrome-192x192.png" alt="logo"></img>
+                {/* <span className="icon">
                     <FontAwesomeIcon icon={faGhost} size="2x" />
-                </span>
+                </span> */}
+                <h4 className="title is-4 px-2">EMFFive.com</h4>
             </a>
 
             <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -20,9 +21,53 @@ const Header = () =>
             </button>
         </div>
 
-        <div className="navbar-menu">
+        <div id="navbarBasicExample" className="navbar-menu">
             <div className="navbar-start">
-                <Link to="/" className="navbar-item">Home</Link>
+                <Link  to="/" className="navbar-item" />
+
+     
+
+                {/* <a className="navbar-item">
+                    Documentation
+                </a>
+
+                <div className="navbar-item has-dropdown is-hoverable">
+                    <a className="navbar-link">
+                        Ghosts
+                    </a>
+                    <div className="navbar-dropdown">
+                        <a className="navbar-item">
+                            About
+                        </a>
+                        <a className="navbar-item">
+                            Jobs
+                        </a>
+                        <a className="navbar-item">
+                            Contact
+                        </a>
+                        <hr className="navbar-divider" />
+                        <a className="navbar-item">
+                            Report an issue
+                        </a>
+                    </div>
+                </div> */}
+            </div>
+
+            <div className="navbar-end">
+
+                <div className="navbar-item">
+                    <a href="https://store.steampowered.com/app/739630/Phasmophobia/" className="navbar-item">
+                        Buy Phasmophobia on Steam
+                    </a>
+                </div>
+
+                <div className="navbar-item">
+                    <div className="buttons">
+                        <a className="button is-primary">
+                            <strong>Buy me a Coffee</strong>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
