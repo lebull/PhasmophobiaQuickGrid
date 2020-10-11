abccc608602efa273386aa810578e36284efd628
lebull <bulldogboy912000@yahoo.com>
2020-10-11 01:45:15 -0500

    Throw in a quick icon at the top of the page

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,17 @@
+import { faGhost } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () =>
     <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-            {/* <a className="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28"></img>
-            </a> */}
+            <a className="navbar-item" href="https://bulma.io">
+            {/* <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28"></img> */}
+                <span className="icon">
+                    <FontAwesomeIcon icon={faGhost} size="2x" />
+                </span>
+            </a>
 
             <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                 <span aria-hidden="true"></span>
